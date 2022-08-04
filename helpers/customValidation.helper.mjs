03a38fa8db548c2e7669714428398a78e0b604fc
Kevin Ashcraft <kevin@kevashcraft.com>
2022-08-04 17:55:19 -0400

    Update to ESM syntax

---
+++ helpers/customValidation.helper.mjs
@@ -0,0 +1,22 @@
+import httpsRequest from "./httpsRequest.helper.mjs";
+
+export default async (request, { hdbCore, logger }) => {
+  const hostname = `jsonplaceholder.typicode.com`;
+  const path = `/todos/1`;
+  const { authorization } = request.headers;
+  const headers = { authorization };
+
+  try {
+    const result = await httpsRequest(hostname, path, headers);
+    if (result.error) {
+      throw new Error(result.error);
+    }
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data ||
+      error.message ||
+      "Sorry, there was an error authenticating your request";
+    logger.error(errorMessage);
+    throw new Error(errorMessage);
+  }
+};

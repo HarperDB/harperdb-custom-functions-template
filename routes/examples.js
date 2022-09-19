32d8f4d686f52ea7a5627f38a95d73194cd00ea8
deliciousmonster <jaxon@deliciousmonster.com>
2022-09-19 10:18:15 -0600

    Update examples.js

--- routes/examples.js
+++ routes/examples.js
@@ -4,21 +4,9 @@ const customValidation = require('../helpers/example');
 
 // eslint-disable-next-line no-unused-vars,require-await
 module.exports = async (server, { hdbCore, logger }) => {
-  // GET, WITH NO preValidation AND USING hdbCore.requestWithoutAuthentication
-  // BYPASSES ALL CHECKS: DO NOT USE RAW USER-SUBMITTED VALUES IN SQL STATEMENTS
-  server.route({
-    url: '/',
-    method: 'GET',
-    handler: (request) => {
-      request.body= {
-        operation: 'sql',
-        sql: 'SELECT * FROM dev.dog ORDER BY dog_name'
-      };
-      return hdbCore.requestWithoutAuthentication(request);
-    }
-  });
 
   // POST, WITH STANDARD PASS-THROUGH BODY, PAYLOAD AND HDB AUTHENTICATION
+  // THIS IS THE SAME AS HITTING THE OPERATIONS API, BUT AT YOUR OWN ENDPOINT
   server.route({
     url: '/',
     method: 'POST',
@@ -26,13 +14,27 @@ module.exports = async (server, { hdbCore, logger }) => {
     handler: hdbCore.request,
   });
 
-
-  // GET, WITH OPERATION SET IN THE preParsing HANDLER
-  // THIS ALLOWS YOU TO DEFINE OPERATIONS SERVER-SIDE
-  // WHILE STILL BEING ABLE TO PREVALIDATE HEADER AUTH
-  // AGAINST THE OPERATION.
+  // GET, USING hdbCore.preValidation AND hdbCore.request
+  // CRAFTING A CUSTOM REQUEST IN THE onRequest HANDLER
   server.route({
-    url: '/:id',
+    url: '/onRequest',
+    method: 'GET',
+    onRequest: (request, reply, done) => {
+      request.body = {
+        operation: 'sql',
+        sql: 'SELECT * FROM dev.dog ORDER BY dog_name'
+      };
+      done();
+    },
+    preValidation: hdbCore.preValidation,
+    handler: hdbCore.request
+  });
+
+  // GET, USING hdbCore.preValidation AND hdbCore.request
+  // CRAFTING A CUSTOM REQUEST IN THE preParsing HANDLER
+  // WITH ACCESS TO request.body (not present in the onRequest handler)
+  server.route({
+    url: '/preParsing/:id',
     method: 'GET',
     preParsing: (request, response, done) => {
       request.body = {
@@ -46,8 +48,24 @@ module.exports = async (server, { hdbCore, logger }) => {
     handler: hdbCore.request,
   });
 
+  // GET, WITH NO preValidation AND USING hdbCore.requestWithoutAuthentication
+  // BYPASSES ALL CHECKS: DO NOT USE RAW USER-SUBMITTED VALUES IN SQL STATEMENTS
+  server.route({
+    url: '/',
+    method: 'GET',
+    onRequest: (request, reply, done) => {
+      request.body = {
+        operation: 'sql',
+        sql: 'SELECT * FROM dev.dog'
+      };
+      done();
+    },
+    handler: (request) => hdbCore.requestWithoutAuthentication(request)
+  });
+
   // GET, WITH ASYNC THIRD-PARTY AUTH PREVALIDATION
   // (IMPORTED FROM ../helpers/example)
+  // BYPASSES ALL CHECKS: DO NOT USE RAW USER-SUBMITTED VALUES IN SQL STATEMENTS
   server.route({
     url: '/async-verify/:id',
     method: 'GET',
@@ -57,11 +75,6 @@ module.exports = async (server, { hdbCore, logger }) => {
         operation: 'sql',
         sql: `SELECT * FROM dev.dog WHERE id = ${request.params.id}`
       };
-
-      /*
-       * requestWithoutAuthentication bypasses the standard HarperDB authentication.
-       * YOU MUST ADD YOUR OWN preValidation method above, or this method will be available to anyone.
-       */
       return hdbCore.requestWithoutAuthentication(request);
     }
   });
